c0827f2d75e9ad9b651f97a0fc9d12b3c0f36dfa
duskalor <duskalor1@gmail.com>
2025-03-11 15:15:59 -0500

    xcvxcvvxd

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import { messageToChat } from './message.js';
 import { getData } from './extractData.js';
 
 getData().then((data) => {
-  console.log('AQUII');
   console.log(data);
-  messageToChat(data);
+  messageToChat(data, 50);
 });
