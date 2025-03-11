032f84b7f66e21cce1e8d3b502cc38e546c4ace4
duskalor <duskalor1@gmail.com>
2025-03-11 18:59:22 -0500

    aaa

--- message.js
+++ message.js
@@ -1,4 +1,4 @@
-import cron from 'node-cron';
+// import cron from 'node-cron';
 import { getData } from './extractData.js';
 // process.loadEnvFile();
 
@@ -8,9 +8,7 @@ export const messageToChat = (time = 10, contador) => {
   let value = time;
   if (value < 10) value = 10;
 
-  return cron.schedule(`*/${value} * * * * *`, async () => {
-    console.log(`con intervalo de ${value} segundos.`);
-
+  const aaa = async () => {
     const text = await getData();
 
     console.log(`scrapping terminado nro ${contador}`);
@@ -27,5 +25,26 @@ export const messageToChat = (time = 10, contador) => {
     } else {
       console.log('error enviando datos a telegram');
     }
-  });
+  };
+  aaa();
+  // return cron.schedule(`*/${value} * * * * *`, async () => {
+  //   console.log(`con intervalo de ${value} segundos.`);
+
+  //   const text = await getData();
+
+  //   console.log(`scrapping terminado nro ${contador}`);
+  //   console.log(`enviando datos a telegram nro ${contador}`);
+  //   const response = await fetch(
+  //     `https://api.telegram.org/bot${TOKEN_BOT}/sendMessage?chat_id=1974797847&text=${encodeURIComponent(
+  //       text
+  //     )}`
+  //   );
+
+  //   if (response.ok) {
+  //     console.log(`datos enviados a telegram exitosamente nro ${contador}`);
+  //     contador++;
+  //   } else {
+  //     console.log('error enviando datos a telegram');
+  //   }
+  // });
 };
