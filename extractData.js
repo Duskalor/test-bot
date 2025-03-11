86a3119e9808ee2269240e27208d0fe5712a9238
duskalor <duskalor1@gmail.com>
2025-03-11 12:23:39 -0500

    test

--- extractData.js
+++ extractData.js
@@ -1,18 +1,11 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
 const URL =
   'https://www.convocatoriasdetrabajo.com/buscar-empleo.php?q=informatica&dep=7';
 
 export const getData = async () => {
   const browser = await puppeteer.launch({
-    headless: true,
-    // executablePath: '/data/data/com.termux/files/usr/bin/firefox',
-    // args: [
-    //   '--no-sandbox',
-    //   '--disable-setuid-sandbox',
-    //   '--disable-gpu', // Deshabilita el uso de GPU
-    //   '--disable-dev-shm-usage', // Evita problemas con memoria compartida
-    // ],
-    // dumpio: true, // Muestra los logs del navegador
+    executablePath: '/usr/bin/chromium-browser',
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 
   const page = await browser.newPage();
