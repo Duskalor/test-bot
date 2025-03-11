d6d1db651a50a3e0de3fdd9085a16f8af46414de
duskalor <duskalor1@gmail.com>
2025-03-11 12:15:04 -0500

    aaaa

--- extractData.js
+++ extractData.js
@@ -1,18 +1,18 @@
-import puppeteer from 'puppeteer-core';
+import puppeteer from 'puppeteer';
 const URL =
   'https://www.convocatoriasdetrabajo.com/buscar-empleo.php?q=informatica&dep=7';
 
 export const getData = async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: '/data/data/com.termux/files/usr/bin/firefox',
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-gpu', // Deshabilita el uso de GPU
-      '--disable-dev-shm-usage', // Evita problemas con memoria compartida
-    ],
-    dumpio: true, // Muestra los logs del navegador
+    // executablePath: '/data/data/com.termux/files/usr/bin/firefox',
+    // args: [
+    //   '--no-sandbox',
+    //   '--disable-setuid-sandbox',
+    //   '--disable-gpu', // Deshabilita el uso de GPU
+    //   '--disable-dev-shm-usage', // Evita problemas con memoria compartida
+    // ],
+    // dumpio: true, // Muestra los logs del navegador
   });
 
   const page = await browser.newPage();
