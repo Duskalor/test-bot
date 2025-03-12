82f451a8694cc5a85dd7c71dbecdf07b1be7cab6
duskalor <duskalor1@gmail.com>
2025-03-11 19:17:19 -0500

    ff

--- message.js
+++ message.js
@@ -8,7 +8,9 @@ export const messageToChat = (time = 10, contador) => {
   let value = time;
   if (value < 10) value = 10;
 
-  const aaa = async () => {
+  return cron.schedule(`*/${value} * * * * *`, async () => {
+    console.log(`con intervalo de ${value} segundos.`);
+
     const text = await getData();
 
     console.log(`scrapping terminado nro ${contador}`);
@@ -25,26 +27,5 @@ export const messageToChat = (time = 10, contador) => {
     } else {
       console.log('error enviando datos a telegram');
     }
-  };
-  aaa();
-  // return cron.schedule(`*/${value} * * * * *`, async () => {
-  //   console.log(`con intervalo de ${value} segundos.`);
-
-  //   const text = await getData();
-
-  //   console.log(`scrapping terminado nro ${contador}`);
-  //   console.log(`enviando datos a telegram nro ${contador}`);
-  //   const response = await fetch(
-  //     `https://api.telegram.org/bot${TOKEN_BOT}/sendMessage?chat_id=1974797847&text=${encodeURIComponent(
-  //       text
-  //     )}`
-  //   );
-
-  //   if (response.ok) {
-  //     console.log(`datos enviados a telegram exitosamente nro ${contador}`);
-  //     contador++;
-  //   } else {
-  //     console.log('error enviando datos a telegram');
-  //   }
-  // });
+  });
 };
