10518a0bf8f41ebeafe4a5805d3c1f9638993b8c
duskalor <duskalor1@gmail.com>
2025-03-20 13:37:21 -0500

    fix prices : It was returning null data

--- src/lib/savePrices.js
+++ src/lib/savePrices.js
@@ -2,15 +2,15 @@ import { tablesDB } from '../utils/const.js';
 import { supabase } from './supabase.js';
 
 export const saveDatabasePrices = async (precios) => {
-  console.log({ precios });
-  if (!precios.compra || !precios.venta)
-    return console.log('datos precio compra o venta erroneo');
+  const compra = parseFloat(precios.compra.split('S/').pop());
+  const venta = parseFloat(precios.venta.split('S/').pop());
+  console.log({ compra, venta });
+  if (!compra || !venta)
+    return console.log('datos precio : compra o venta es undefined');
+
   const { error: ExchangeRateError } = await supabase
     .from(tablesDB.ExchangeRate)
-    .insert({
-      compra: parseFloat(precios.compra.split('S/').pop()),
-      venta: parseFloat(precios.venta.split('S/').pop()),
-    });
+    .insert({ compra, venta });
 
   if (ExchangeRateError)
     return console.error('Error al crear', ExchangeRateError);
